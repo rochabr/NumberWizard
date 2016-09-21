443c238cfff483cecd47a3df64bc6cc2b002f81a
Fernando Rocha <fernandorsl@gmail.com>
2016-09-21 13:40:37 -0500

    Adding assets and project settings

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizards : MonoBehaviour {
+
+	public Text guessText;
+	
+	int max = 1000;
+	int min = 1;
+	int guess;
+	
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		max = max + 1;
+		NextGuess();
+	}
+
+	public void GuessHigher(){
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower(){
+		max = guess;
+		NextGuess ();
+	}
+
+	public void GuessCorrect(){
+		StartGame ();
+	}
+	
+	void NextGuess () {
+		//guess = (max + min) / 2;
+		guess = Random.Range(min, max);
+		print ("Next guess is " + guess);
+		guessText.text = guess.ToString();
+	}
+}
